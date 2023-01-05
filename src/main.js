22d193abd16d0ca0ec81978721cd2ad51512a332
Guiye Ensinck <guiye.ensinck@hoopla.la>
2023-01-05 09:16:13 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import SplitType from "split-type";
-
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Split text into spans
-  let typeSplit = new SplitType("[text-split]", {
-    types: "words, chars",
-    tagName: "span"
-  })
